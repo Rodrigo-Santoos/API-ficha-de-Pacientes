bdd0d7dffe041f5b55a0c7e0a4f76deb48626578
Rodrigo Oliveira <rodrigoyukimide@gmail.com>
2021-08-31 14:46:19 -0300

    API Pacientes

---
+++ pacientes/src/main/java/com/paciente/pacientes/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.paciente.pacientes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paciente.pacientes.models.Pacientes;
+
+public interface PacienteRepository extends JpaRepository<Pacientes, Long> {
+
+}
---
+++ pacientes/src/main/java/com/paciente/pacientes/resources/PacientesResources.java
@@ -0,0 +1,27 @@
+package com.paciente.pacientes.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paciente.pacientes.models.Pacientes;
+import com.paciente.pacientes.repository.PacienteRepository;
+
+@RestController
+@RequestMapping(value="/api")
+public class PacientesResources {
+	
+	@Autowired
+	PacienteRepository pacienteRepository;
+	
+	
+	@GetMapping("/pacientes")
+	public List<Pacientes> listarPacientes(){
+		return pacienteRepository.findAll();
+		
+	}
+
+}
