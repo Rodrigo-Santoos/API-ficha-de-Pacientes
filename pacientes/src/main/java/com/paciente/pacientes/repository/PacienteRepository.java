a7f5d548550ddc370f20d11246a2a2d5f9d17c91
Rodrigo Oliveira <rodrigoyukimide@gmail.com>
2021-09-01 15:14:33 -0300

    Pequena a atualizaçao de consulta pela id

--- pacientes/src/main/java/com/paciente/pacientes/repository/PacienteRepository.java
+++ pacientes/src/main/java/com/paciente/pacientes/repository/PacienteRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.paciente.pacientes.models.Pacientes;
 
 public interface PacienteRepository extends JpaRepository<Pacientes, Long> {
+	
+	Pacientes findById(long id);
 
 }
--- pacientes/src/main/java/com/paciente/pacientes/resources/PacientesResources.java
+++ pacientes/src/main/java/com/paciente/pacientes/resources/PacientesResources.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,19 @@ public class PacientesResources {
 		return pacienteRepository.findAll();
 		
 	}
+	
+	@GetMapping("/paciente/{id}")
+	public Pacientes listarUnicoPacientes(@PathVariable(value="id") long id){
+		return pacienteRepository.findById(id);
+		
+	}
+	
+	@PostMapping("/paciente")
+	public Pacientes salvarPacientes(@RequestBody Pacientes paciente){
+		return pacienteRepository.save(paciente);
+		
+	}
+	
+	
 
 }
