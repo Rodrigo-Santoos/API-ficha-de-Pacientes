d72fbfd703155beafff9d494011cbb7664f710c2
Rodrigo Oliveira <rodrigoyukimide@gmail.com>
2021-09-27 13:25:59 -0300

    Delete e Put

--- pacientes/src/main/java/com/paciente/pacientes/resources/PacientesResources.java
+++ pacientes/src/main/java/com/paciente/pacientes/resources/PacientesResources.java
@@ -3,9 +3,11 @@ package com.paciente.pacientes.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,24 +22,39 @@ public class PacientesResources {
 	@Autowired
 	PacienteRepository pacienteRepository;
 	
-	
+	//trazendo todos
 	@GetMapping("/pacientes")
 	public List<Pacientes> listarPacientes(){
 		return pacienteRepository.findAll();
 		
 	}
 	
-	@GetMapping("/paciente/{id}")
+	//trazendo um produto pela id 
+	@GetMapping("/pacientes/{id}")
 	public Pacientes listarUnicoPacientes(@PathVariable(value="id") long id){
 		return pacienteRepository.findById(id);
 		
 	}
 	
-	@PostMapping("/paciente")
+	//Salvando o Produto via postman no banco de Dados
+	@PostMapping("/pacientes")
 	public Pacientes salvaPaciente(@RequestBody Pacientes paciente){
 		return pacienteRepository.save(paciente);
 		
 	}
+	
+	@DeleteMapping("/pacientes")
+	public void deletarPaciente(@RequestBody Pacientes paciente){
+		pacienteRepository.delete(paciente);
+		
+	}
+	
+	@PutMapping("/pacientes")
+	public Pacientes atualizarPaciente(@RequestBody Pacientes paciente){
+		return pacienteRepository.save(paciente);
+		
+	}
+	
 	
 	
 
